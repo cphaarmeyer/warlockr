26441f70436593821020f4b4b68a3d39f13ded60
Philipp Haarmeyer <philipp.haarmeyer@hotmail.de>
2020-01-24 21:23:21 +0100

    add sim dps function

---
+++ R/sim_dps.R
@@ -0,0 +1,16 @@
+#' Simultate DPS
+#'
+#' Repeatedly simulates bosses to calculate dps.
+#'
+#' @inherit sim_boss
+#' @param iter number of iterations
+#'
+#' @return a matrix where every row is a vector returned by \code{sim_boss}
+#' @export
+#'
+#' @examples
+#' mat <- sim_dps(2, 1, 277, 346, 5300, 150, 1000)
+#' mean(mat[, 4])
+sim_dps <- function(crit, hit, int, sp, mana, time, iter = 50000) {
+  t(replicate(iter, sim_boss(crit, hit, int, sp, mana, time)))
+}
