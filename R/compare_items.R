2fb4e996ac6dd981ca69a00192819cdb56fbbdfb
Philipp Haarmeyer <philipp.haarmeyer@hotmail.de>
2020-01-25 20:09:31 +0100

    improve compare item function to work with multiple items at a time

--- R/compare_items.R
+++ R/compare_items.R
@@ -3,23 +3,30 @@
 #' Simulates dps with current stats and with new stats to compare them.
 #'
 #' @inheritParams sim_dps
-#' @param d_crit difference in crit
-#' @param d_hit difference in hit
-#' @param d_int difference in intelligence
-#' @param d_sp difference in spellpower
-#' @param d_mp5 difference in mp5
+#' @param items list where each element indicates the stat changes by an item
 #'
-#' @return a vector with current dps, new dps and difference
+#' @return a data frame with current dps, new dps and difference
 #' @export
 #'
 #' @examples
-#' compare_items(2, 1, 277, 346, 0, -1, 1, 5, 3, 0, iter = 1000)
-compare_items <- function(crit, hit, int, sp, mp5, d_crit = 0, d_hit = 0, d_int = 0, d_sp = 0, d_mp5 = 0, timeframe = c(45, 150), iter = 50000) {
+#' compare_items(2, 1, 277, 346, 0,
+#'   items = list(
+#'     new_item1 = list(crit = -1, hit = 1),
+#'     new_item2 = list(int = 5, sp = 3)
+#'   ),
+#'   iter = 1000
+#' )
+compare_items <- function(crit, hit, int, sp, mp5, items, timeframe = c(45, 150), iter = 50000) {
   seed <- sample(1:1000, 1)
-  simulations <- list(
-    current = sim_dps(crit, hit, int, sp, mp5, NULL, timeframe, iter, seed),
-    new = sim_dps(crit + d_crit, hit + d_hit, int + d_int, sp + d_sp, mp5 + d_mp5, NULL, timeframe, iter, seed)
-  )
-  dps <- vapply(simulations, function(x) mean(x[, 4]), FUN.VALUE = 0)
-  c(dps, c(diff = dps[[2]] - dps[[1]]))
+  to_0 <- function(x) if (is.null(x)) 0 else x
+  current <- sim_dps(crit, hit, int, sp, mp5, NULL, timeframe, iter, seed)
+  simulations <- lapply(items, function(x) {
+    sim_dps(
+      crit + to_0(x$crit), hit + to_0(x$hit), int + to_0(x$int), sp + to_0(x$sp), mp5 + to_0(x$mp5),
+      NULL, timeframe, iter, seed
+    )
+  })
+  dps <- mean(current[, 4])
+  new_dps <- vapply(c(list(current = current), simulations), function(x) mean(x[, 4]), FUN.VALUE = 0)
+  data.frame(dps = new_dps, diff = new_dps - dps)
 }
