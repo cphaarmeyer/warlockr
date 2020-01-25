0f928ec61a21e340c077b940419e3c7c38f36c0f
Philipp Haarmeyer <philipp.haarmeyer@hotmail.de>
2020-01-25 01:41:13 +0100

    implement low level sim boss function

--- R/sim_boss.R
+++ R/sim_boss.R
@@ -14,8 +14,6 @@
 #' sim_boss(2, 1, 277, 346)
 sim_boss <- function(crit, hit, int, sp, mana = NULL, time = 150) {
   if (is.null(mana)) mana <- compute_mana(int)
-  stats_total <- c(0, mana, 0, 0)
-  sim_row <- c(0, 0, 0, 0)
   n_max <- time %/% 2.5 + 1
   sb_dmg <- (sample_shadowbolt(n_max) + 0.8571 * sp) * 1.265
   sample_h <- sample_hit(n_max)
@@ -23,18 +21,5 @@ sim_boss <- function(crit, hit, int, sp, mana = NULL, time = 150) {
   sb_crit <- sample_h >= (100 - compute_critchance(crit, int))
   sb_manacost <- compute_manacost()
   lt_mancost <- compute_manacost("lifetap", sp = sp)
-
-  i <- 0
-  while (stats_total[3] < time) {
-    if (stats_total[2] < sb_manacost) {
-      sim_row <- lifetap_impl(lt_mancost, sim_row[4])
-    } else {
-      i <- i + 1
-      sim_row <- shadowbolt_impl(sb_dmg[i], sb_miss[i], sb_crit[i], -sb_manacost, 2.5, 1, 5, sim_row[4])
-    }
-    stats_total <- stats_total + sim_row
-  }
-
-  stats_total[4] <- stats_total[1] / stats_total[3]
-  return(as.numeric(stats_total))
+  sim_boss_impl(mana, sb_dmg, sb_miss, sb_crit, sb_manacost, lt_mancost, time)
 }
---
+++ R/sim_boss_impl.R
@@ -0,0 +1,26 @@
+#' Base Implementation Simulate Boss Fight
+#'
+#' @inheritParams sim_boss
+#' @param sb_dmg vector of shadow bolt damage ignoring possible miss or crit
+#' @param sb_miss vector of whether shadow bolt hits
+#' @param sb_crit vector of whether shadow bolt crits
+#' @param sb_manacost mana cost of shadow bolt
+#' @param lt_mancost mana cost of life tap
+#'
+#' @export
+sim_boss_impl <- function(mana, sb_dmg, sb_miss, sb_crit, sb_manacost, lt_mancost, time) {
+  stats_total <- c(0, mana, 0, 0)
+  sim_row <- c(0, 0, 0, 0)
+  i <- 0
+  while (stats_total[3] < time) {
+    if (stats_total[2] < sb_manacost) {
+      sim_row <- lifetap_impl(lt_mancost, sim_row[4])
+    } else {
+      i <- i + 1
+      sim_row <- shadowbolt_impl(sb_dmg[i], sb_miss[i], sb_crit[i], -sb_manacost, 2.5, 1, 5, sim_row[4])
+    }
+    stats_total <- stats_total + sim_row
+  }
+  stats_total[4] <- stats_total[1] / stats_total[3]
+  stats_total
+}
