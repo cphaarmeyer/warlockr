8af7c8ceb2685cc0b8ec1f9a5c877e05cba0a6ef
Philipp Haarmeyer <philipp.haarmeyer@hotmail.de>
2020-01-28 01:55:19 +0100

    add curse at the start of a boss simulation

--- R/sim_boss_impl.R
+++ R/sim_boss_impl.R
@@ -4,18 +4,20 @@
 #' @param sb_dmg vector of shadow bolt damage ignoring possible miss or crit
 #' @param sb_miss vector of whether shadow bolt hits
 #' @param sb_crit vector of whether shadow bolt crits
+#' @param curse_miss vector of whether curse hits
 #' @param sb_manacost mana cost of shadow bolt
 #' @param lt_mancost mana cost of life tap
 #'
 #' @export
-sim_boss_impl <- function(mana, mp5, sb_dmg, sb_miss, sb_crit, sb_manacost, lt_mancost, time) {
-  stats_total <- c(0, mana, 0, 0)
+sim_boss_impl <- function(mana, mp5, sb_dmg, sb_miss, sb_crit, curse_miss, sb_manacost, lt_mancost, time) {
+  n_curse <- min(which(!curse_miss))
+  stats_total <- c(0, mana - n_curse * 200, 1.5 * n_curse, 0)
   sim_row <- c(0, 0, 0, 0)
   i <- 0
   j <- 1
-  t_mp5 <- seq(2.5, time + 5, by = 5)
+  t_mp5 <- seq(0, time + 5, by = 5)
   while (stats_total[3] < time) {
-    if (stats_total[3] > t_mp5[j]) {
+    if (stats_total[3] >= t_mp5[j]) {
       j <- j + 1
       stats_total[2] <- stats_total[2] + mp5
     }
--- R/sim_setup.R
+++ R/sim_setup.R
@@ -3,6 +3,7 @@
 #' @param n sample number
 #' @inheritParams shadowbolt
 #' @inheritParams sim_dps
+#' @param suppression number of talent points in suppression
 #'
 #' @return a list of arguments
 #' @export
@@ -11,7 +12,7 @@
 #' sim_setup(10, 2, 1, 277, 346, NULL)
 sim_setup <- function(n, crit, hit, int, sp, mana, iter = 1,
                       devastation = 5, ruin = 1, cataclysm = 2, shadow_mastery = 0,
-                      demonic_sacrifice = 1, curse_of_shadows = 1) {
+                      demonic_sacrifice = 1, curse_of_shadows = 1, suppression = 2) {
   if (is.null(mana)) mana <- compute_mana(int)
   to_matrix <- function(x) {
     if (iter > 1) matrix(x, ncol = iter) else x
@@ -20,15 +21,18 @@ sim_setup <- function(n, crit, hit, int, sp, mana, iter = 1,
     (1 + 0.15 * demonic_sacrifice) * (1 + 0.1 * curse_of_shadows)
   sample_h <- sample_hit(n * iter)
   sample_c <- sample_hit(n * iter)
-  sb_miss <- (sample_h <= 1 | sample_h <= (17 - hit))
+  sample_curse <- sample_hit(n * iter)
+  sb_miss <- sample_h <= 1 | sample_h <= (17 - hit)
   sb_crit <- sample_c >= (100 - compute_critchance(crit, int, devastation))
   sb_dmg <- (sb_dmg * !sb_miss) * (1 + sb_crit * 0.5 * (1 + ruin))
+  curse_miss <- sample_curse <= 1 | sample_curse <= (17 - hit - 2 * suppression)
   list(
     mana = mana,
     sb_dmg = to_matrix(sb_dmg),
     sb_miss = to_matrix(sb_miss),
     sb_crit = to_matrix(sb_crit),
     sb_manacost = -compute_manacost(cataclysm = cataclysm),
-    lt_mancost = compute_manacost("lifetap", sp = sp)
+    lt_mancost = compute_manacost("lifetap", sp = sp),
+    curse_miss = to_matrix(curse_miss)
   )
 }
