95ab72f50b53d0b0a8477f02c15c864635b44ff0
Philipp Haarmeyer <philipp.haarmeyer@hotmail.de>
2020-03-15 15:03:22 +0100

    test sample_shadowbolt

---
+++ tests/testthat/test_sample_shadowbolt.R
@@ -0,0 +1,10 @@
+context("sample_shadowbolt")
+
+
+test_that("sample_shadowbolt works as expected", {
+  d <- sample_shadowbolt(50000)
+
+  expect_length(d, 50000)
+  expect_true(all(d >= 455))
+  expect_true(all(d <= 507))
+})
