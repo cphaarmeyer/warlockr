ec5a35da20f8cfe9f3d1c64a63dffb66bd39f748
Philipp Haarmeyer <philipp.haarmeyer@hotmail.de>
2020-03-15 15:23:45 +0100

    test compute_critchance

---
+++ tests/testthat/test_compute_critchance.R
@@ -0,0 +1,15 @@
+context("compute_critchance")
+
+
+test_that("compute_critchance works as expected", {
+  c1 <- compute_critchance(3, 267)
+  c2 <- compute_critchance(4, 267)
+  c3 <- compute_critchance(3, 268)
+  c4 <- compute_critchance(3, 267, devastation = 0)
+
+  expect_length(c1, 1)
+  expect_type(c1, "double")
+  expect_gt(c2, c1)
+  expect_gt(c3, c1)
+  expect_lt(c4, c1)
+})
