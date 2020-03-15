c0b048eac47ad39767a9693e720ab3d44fca7003
Philipp Haarmeyer <philipp.haarmeyer@hotmail.de>
2020-03-15 15:42:34 +0100

    test lifetap

---
+++ tests/testthat/test_lifetap.R
@@ -0,0 +1,12 @@
+context("lifetap")
+
+
+test_that("lifetap works as expected", {
+  lt <- lifetap(346)
+
+  expect_length(lt, 4)
+  expect_type(lt, "double")
+  expect_gt(lt[[2]], 0)
+  expect_true(lt[[3]] == 1.5)
+  expect_true(all(lt[c(1, 4)] == 0))
+})
