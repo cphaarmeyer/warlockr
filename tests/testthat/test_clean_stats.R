b1573ec27e2cf4fbbd198db866b539b7fc882010
Philipp Haarmeyer <philipp.haarmeyer@hotmail.de>
2020-03-15 15:18:57 +0100

    simplify clean_stats test

--- tests/testthat/test_clean_stats.R
+++ tests/testthat/test_clean_stats.R
@@ -6,15 +6,13 @@ test_that("clean_stats works as expected", {
   s1 <- c(s, list(a = 2))
   s2 <- s[c("int", "crit")]
   s3 <- s[c("crit", "hit", "int", "sp")]
+  res <- list(int = 277, sp = 346, crit = 2, hit = 2, mp5 = 0)
 
-  expect_identical(
-    clean_stats(s),
-    list(int = 277, sp = 346, crit = 2, hit = 2, mp5 = 0)
-  )
-  expect_identical(clean_stats(s1), clean_stats(s))
+  expect_identical(clean_stats(s), res)
+  expect_identical(clean_stats(s1), res)
   expect_identical(
     clean_stats(s2),
     list(int = 277, sp = 0, crit = 2, hit = 0, mp5 = 0)
   )
-  expect_identical(clean_stats(s3), clean_stats(s))
+  expect_identical(clean_stats(s3), res)
 })
