8e261b6902b573c0093e65eb5e31e964d273c0aa
Philipp Haarmeyer <philipp.haarmeyer@hotmail.de>
2020-04-23 21:03:55 +0200

    adjust sp_bonus processing
    Merge branch 'master' into rccp

--- R/sim_boss_impl.R
+++ R/sim_boss_impl.R
@@ -20,13 +20,10 @@ sim_boss_impl <- function(mana, mp5, sp, sb_dmg, sb_miss, sb_crit, curse_miss,
   sb <- sim_data[sim_data[, 3] == 2.5, ]
   nsb <- nrow(sb)
   if (!is.null(sp_bonus)) {
-    rows <- lapply(lengths(sp_bonus), sample.int, n = nsb)
-    lst <- lapply(names(sp_bonus), function(x) {
-      out <- double(nsb)
-      out[rows[[x]]] <- sp_bonus[[x]]
-      out
-    })
-    sp <- sp + do.call(`+`, lst)
+    rows <- sample.int(nsb, length(sp_bonus))
+    out <- double(nsb)
+    out[rows] <- sp_bonus
+    sp <- sp + out
   }
   dmg <- shadowbolt_dmg(
     sb[, 1], sp, sb_miss[seq_len(nsb)], sb_crit[seq_len(nsb)],
