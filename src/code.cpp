fd6195e52502798622dc3b285ecb43c885153ae9
Philipp Haarmeyer <philipp.haarmeyer@hotmail.de>
2020-04-23 11:05:29 +0200

    set up sim_boss_loop

--- src/code.cpp
+++ src/code.cpp
@@ -1,2 +1,16 @@
 #include <Rcpp.h>
 using namespace Rcpp;
+
+//' Simulate the Loop for a Boss Fight
+//'
+//' @inheritParams sim_boss_impl
+//'
+//' @export
+// [[Rcpp::export]]
+NumericMatrix sim_boss_loop(int mana, int mp5, NumericVector sb_dmg,
+                            LogicalVector sb_miss, LogicalVector sb_crit,
+                            LogicalVector curse_miss, double sb_manacost,
+                            double lt_manacost, double time) {
+  NumericMatrix mat(10, 4);
+  return mat;
+}
