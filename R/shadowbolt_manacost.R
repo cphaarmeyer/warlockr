16279e6a5d25e1f2f7f477d78f2659728983ec23
Philipp Haarmeyer <philipp.haarmeyer@hotmail.de>
2020-05-03 14:49:11 +0200

    shadowbolt_manacost

---
+++ R/shadowbolt_manacost.R
@@ -0,0 +1,11 @@
+#' Shadow Bolt Mana Cost
+#'
+#' @inheritParams shadowbolt
+#'
+#' @export
+#'
+#' @examples
+#' shadowbolt_manacost()
+shadowbolt_manacost <- function(cataclysm = 2) {
+  370 * (1 - 0.01 * cataclysm)
+}
