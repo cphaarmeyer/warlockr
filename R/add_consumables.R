523986ccb5c0a500ed1a62dc4684849143b12808
Philipp Haarmeyer <philipp.haarmeyer@hotmail.de>
2020-08-30 00:33:56 +0200

    add_consumables

---
+++ R/add_consumables.R
@@ -0,0 +1,27 @@
+#' Add Consumables
+#'
+#' Adds consumables to stats.
+#' Available are:
+#' Greater Arcane Elixir (gae),
+#' Elixir of Shadow Power (eosp),
+#' Brilliant Wizard Oil (bwo),
+#' Flask of Supreme Power (fosp)
+#'
+#' @inheritParams compare_equip
+#' @param consumables shortcuts of consumables
+#'
+#' @export
+#'
+#' @examples
+#' add_consumables(list(int = 275, sp = 581, crit = 5, hit = 9))
+add_consumables <- function(stats, consumables = c("gae", "eosp", "bwo")) {
+  stopifnot(consumables %in% names(available_consumables))
+  sum_stats(c(list(stats), available_consumables[consumables]))
+}
+
+available_consumables <- list(
+  gae = list(sp = 35),
+  eosp = list(sp = 40),
+  bwo = list(sp = 30, crit = 1),
+  fosp = list(sp = 150)
+)
