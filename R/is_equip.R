96cd13f60c09e4100954a7be1014516d5d4b98ec
Philipp Haarmeyer <philipp.haarmeyer@hotmail.de>
2020-09-05 21:33:09 +0200

    is_equip checks list elements

--- R/is_equip.R
+++ R/is_equip.R
@@ -15,9 +15,14 @@ is_equip <- function(equip) {
     "hands", "waist", "legs", "feet", "finger1", "finger2",
     "trinket1", "trinket2", "weapon", "wand"
   )
-  is_unique(names(equip)) & setequal(names(equip), slots)
+  all_slots_valid <- do.call(what = all, args = lapply(equip, slot_is_valid))
+  is_unique(names(equip)) & setequal(names(equip), slots) & all_slots_valid
 }
 
 is_unique <- function(x) {
   anyDuplicated(x) == 0
+}
+
+slot_is_valid <- function(x) {
+  is_stats(x) | (is.character(x) & length(x) == 1)
 }
