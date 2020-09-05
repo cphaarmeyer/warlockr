e460519f84e80a1d653d7e0a36bd4deafb2c10f5
Philipp Haarmeyer <philipp.haarmeyer@hotmail.de>
2020-09-05 22:56:03 +0200

    refactor compare_buffs

--- R/compare_buffs.R
+++ R/compare_buffs.R
@@ -14,16 +14,13 @@ compare_buffs <- function(stats,
                           worldbuffs = character(),
                           timeframe = c(60, 300),
                           iter = 50000) {
+  with_buffs_trimmed <- function(...) with_buffs(..., buffs = character())
+  with_consumables <- function(nm) with_buffs_trimmed(stats, consumables = nm)
+  with_worlbuffs <- function(nm) with_buffs_trimmed(stats, worldbuffs = nm)
   stats_list <- c(
     list(current = stats),
-    lapply(
-      char_set_names(consumables),
-      function(nm) with_buffs(stats, buffs = character(), consumables = nm)
-    ),
-    lapply(
-      char_set_names(worldbuffs),
-      function(nm) with_buffs(stats, buffs = character(), worldbuffs = nm)
-    )
+    lapply(char_set_names(consumables), with_consumables),
+    lapply(char_set_names(worldbuffs), with_worlbuffs)
   )
   compare_dps(stats_list, timeframe = timeframe, iter = iter)
 }
