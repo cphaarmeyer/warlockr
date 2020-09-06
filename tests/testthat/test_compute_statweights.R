e75d3655d0fa3a1ca0c27d0d889bd03970dfd7de
Philipp Haarmeyer <philipp.haarmeyer@hotmail.de>
2020-09-06 22:15:32 +0200

    test seed in compute_statweights

--- tests/testthat/test_compute_statweights.R
+++ tests/testthat/test_compute_statweights.R
@@ -2,14 +2,16 @@ context("compute_statweights")
 
 
 test_that("compute_statweights works as expected", {
-  weights <- compute_statweights(
-    list(int = 277, sp = 346, crit = 2, hit = 2),
-    iter = 1000
-  )
+  stats <- list(int = 277, sp = 346, crit = 2, hit = 2)
+  weights <- compute_statweights(stats, iter = 1000)
+  seed <- sample_seed()
+  w_seed1 <- compute_statweights(stats, iter = 1000, seed = seed)
+  w_seed2 <- compute_statweights(stats, iter = 1000, seed = seed)
 
   expect_s3_class(weights, "data.frame")
   expect_named(weights, c("weight", "dps"))
   expect_identical(row.names(weights), c("int", "sp", "crit", "hit", "mp5"))
   expect_type(weights[["weight"]], "double")
   expect_true(weights[["sp", "weight"]] == 1)
+  expect_identical(w_seed1, w_seed2)
 })
