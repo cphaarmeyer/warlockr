4033542445f3cd7f633c3c5e23884453384b683e
Philipp Haarmeyer <philipp.haarmeyer@hotmail.de>
2020-09-14 13:22:29 +0200

    refactor sim_setup

--- R/sim_setup.R
+++ R/sim_setup.R
@@ -11,22 +11,24 @@ sim_setup <- function(times, crit, hit, int, sp,
                       iter = 1,
                       talents = warlock_talents()) {
   stopifnot(length(times) == iter)
-  n <- max(times) %/% 2.5 + 1
-  to_matrix <- function(x) {
-    if (iter > 1) matrix(x, ncol = iter) else x
-  }
-  s <- sample_sim(n * iter)
-  sb_miss <- compute_miss(s$hit, hit)
-  sb_crit <- s$crit >= (100 - compute_critchance(crit, int, talents))
-  curse_miss <- compute_miss(s$curse, hit + 2 * talents[["suppression"]])
+  max_n_shadowbolts <- max(times) %/% 2.5 + 1
+  s <- lapply(sample_sim(max_n_shadowbolts * iter), to_matrix, ncol = iter)
   list(
     mana = compute_mana(int),
-    sb_dmg = to_matrix(s$sb),
-    sb_miss = to_matrix(sb_miss),
-    sb_crit = to_matrix(sb_crit),
+    sb_dmg = s$sb,
+    sb_miss = is_miss(s$hit, hit),
+    sb_crit = is_crit(s$crit, crit, int, talents),
     sb_mana = shadowbolt_mana(talents),
     lt_mana = lifetap_mana(sp = sp),
-    curse_miss = to_matrix(curse_miss),
+    curse_miss = is_miss(s$curse, hit + 2 * talents[["suppression"]]),
     sp_bonus = if (!is.null(trinkets)) trinket_sp(trinkets, times)
   )
+}
+
+to_matrix <- function(x, ncol) if (ncol > 1) matrix(x, ncol = ncol) else x
+
+is_miss <- function(s, hit) s <= 1 | s <= (17 - hit)
+
+is_crit <- function(s, crit, int, talents) {
+  s >= (100 - compute_critchance(crit, int, talents))
 }
